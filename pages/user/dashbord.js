5db8a207c3c35df14697ff1674e5547ffa8a2a61
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-11-17 16:39:26 -0300

    criando card para produto na page home do user

--- pages/user/dashbord.js
+++ pages/user/dashbord.js
@@ -2,7 +2,12 @@ import TemplateDefault from '../../src/templetes/Default'
 import { 
   Container,
   Typography,
-  Button
+  Button,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
 } from '@mui/material';
 
 import { makeStyles } from '@mui/styles';
@@ -11,9 +16,15 @@ const useStyles = makeStyles((theme)=> ({
   container:{
     padding: theme.spacing(8,0,6),
     },
-  buttonAdd:{
+  button:{
     margin:'30px auto',
     display:'block',
+  },
+  cards:{
+    margin:5
+  },
+  cardMedia:{
+    paddingTop: '56%',
   }
 }))
 
@@ -25,10 +36,78 @@ export default function Home() {
         <Typography component={'h1'} variant="h2" align='center'>
           Meus Anúncios
         </Typography>
-        <Button variant='contained' color='primary' className={classes.buttonAdd}>
+        <Button variant='contained' color='primary' className={classes.button}>
           Publicar novo anúncio
         </Button>
 
+      </Container>
+      <Container maxWidth="md"> 
+        <Grid container spacing={4}>
+          
+          <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  image={'https://source.unsplash.com/random'}
+                  alt="green iguana"
+                 />
+                <CardContent>
+                  <Typography variant='h5' component={'h2'}>
+                    Produto x
+                  </Typography>
+                  <Typography>
+                    R$ 60,00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small' color='primary'>Editar</Button>
+                  <Button size='small' color='primary'>Remover</Button>
+                </CardActions>
+              </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  image={'https://source.unsplash.com/random'}
+                  alt="green iguana"
+                 />
+                <CardContent>
+                  <Typography variant='h5' component={'h2'}>
+                    Produto x
+                  </Typography>
+                  <Typography>
+                    R$ 60,00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small' color='primary'>Editar</Button>
+                  <Button size='small' color='primary'>Remover</Button>
+                </CardActions>
+              </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  image={'https://source.unsplash.com/random'}
+                  alt="green iguana"
+                 />
+                <CardContent>
+                  <Typography variant='h5' component={'h2'}>
+                    Produto x
+                  </Typography>
+                  <Typography>
+                    R$ 60,00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small' color='primary'>Editar</Button>
+                  <Button size='small' color='primary'>Remover</Button>
+                </CardActions>
+              </Card>
+          </Grid>
+        </Grid>
       </Container>
     </TemplateDefault>
   );
