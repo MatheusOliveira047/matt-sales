65376bdf619ffbd5b99943b6320ace5dbad6ca42
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-11-27 20:16:09 -0300

    refatorando API com next-connect e criando controller de users

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,37 +1,9 @@
-import dbConnect from '../../src/utils/dbConnect'
-import { crypto } from '../../src/utils/password'
+import nextConnect from 'next-connect'
+import {get,post} from '../../src/controllers/users'
 
-import UsersModel from '../../src/models/users'
+const route = nextConnect()
 
-const users = async (req,res)=>{
-  const {method} = req
+route.get(get)
+route.post(post)
 
-  switch(method){
-    case 'GET':
-      await dbConnect()
-      res.status(200).json({success: true})
-      break
-
-    case 'POST':
-    const { 
-      name,
-      email,
-      password,
-    } = req.body
-
-    await dbConnect()
-
-    const passwordCrypto = await crypto(password)
-
-    const user = new UsersModel({
-      name,
-      email,
-      password: passwordCrypto
-    })
-
-    user.save()
-    res.status(200).json({success: true})
-  }
-}
-
-export default users
+export default route
