4216ff4761d510b3890b11c3372b402532c65f08
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-11-27 18:41:30 -0300

    criando rota de cadrastro de usuários na API

---
+++ src/models/users.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: [true, 'O Campo "Nome" é obrigatório']
+  },
+  email:{
+    type: String,
+    required: [true, 'O Campo "Email" é obrigatório']
+  },
+  password:{
+    type: String,
+    required: [true, 'O Campo "Senha" é obrigatório']
+  },
+
+})
+
+export default mongoose.models.users || mongoose.model('users', schema)
+
